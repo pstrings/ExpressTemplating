34fe9b81ea53d413c310439fd54fc72003a149ae
Suyash <paradisestrings99@gmail.com>
2022-07-28 19:47:22 +0530

    Added Pug templates and passed data to it

--- app.js
+++ app.js
@@ -9,14 +9,17 @@ const shopRouter = require("./routes/shop")
 
 const app = express()
 
-app.use(bodyParser.urlencoded({extended: false}))
+// Setting pug as the templating engine
+app.set('view engine', 'pug') 
+
+app.use(bodyParser.urlencoded({extended: false})) 
 app.use(express.static(path.join(__dirname, "public")))
 
 app.use("/admin", adminData.routes)
 app.use(shopRouter)
 
 app.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, "views", "404.html"))
+  res.status(404).render('404')
 })
 
 app.listen(3000)
