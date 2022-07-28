7357e3df74ebfaa6cf1cdd35ce4676bc4a3deaff
Suyash <paradisestrings99@gmail.com>
2022-07-28 23:41:35 +0530

    Added layouts for common portion of templates

--- routes/shop.js
+++ routes/shop.js
@@ -8,7 +8,7 @@ const router = express.Router()
 
 router.get("/", (req, res, next) => {
     const products = adminData.products
-    res.render('shop', {prods: products, docTitle: "My Shop"})
+    res.render('shop', {prods: products, pageTitle: "My Shop", path: "/"})
 })
 
 module.exports = router
